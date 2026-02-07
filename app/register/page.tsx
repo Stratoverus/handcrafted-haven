67c0a42382db494f0973748a2f78b5ea7817341f
Kevin G. Ferreira <kevingarciaf.kf@outlook.com>
2026-02-06 21:48:54 -0300

    changing to pnpm

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,7 @@ export default function Register(){
     return(
         <section>
 
-            <div className="grid grid-cols-1 min-h-screen bg-gradient-to-br from-[#CF5C36] via-[#EFC88B] via-[#F4E3B2] to-[#FFF]">
+            <div className="grid grid-cols-1 min-h-screen">
                 <RegisterForm />
             </div>
 
