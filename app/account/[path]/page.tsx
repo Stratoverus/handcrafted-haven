c49c5acfb2a1ae34c152be1d2f80e21700aa62e4
jneberhard <jreberhard3@gmail.com>
2026-02-11 15:46:49 -0700

    update wednesday

--- app/account/[path]/page.tsx
+++ app/account/[path]/page.tsx
@@ -7,7 +7,7 @@ export function generateStaticParams() {
   return Object.values(accountViewPaths).map((path) => ({ path }));
 }
 
-export default async function AccountPage({ params }: { params: Promise<{ path: string }> }) {
+/*export default async function AccountPage({ params }: { params: Promise<{ path: string }> }) {
   const { path } = await params;
 
   return (
@@ -15,4 +15,4 @@ export default async function AccountPage({ params }: { params: Promise<{ path: 
       <AccountView path={path} />
     </main>
   );
-}
+}*/
